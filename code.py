497152054997f3003fe1864468ab00b368ea2d3d
Dhyan Tanna <79247409+D99-1@users.noreply.github.com>
2024-08-07 22:55:11 +1000

    Update code.py

--- code.py
+++ code.py
@@ -9,11 +9,14 @@ from kmk.scanners import DiodeOrientation
 keyboard = KMKKeyboard()
 
 keyboard.col_pins = (board.GP3,board.GP4,board.GP5)
-keyboard.row_pins = (board.GP6,)
-keyboard.diode_orientation = DiodeOrientation.COL2ROW
+keyboard.row_pins = (board.GP6,board.GP7,board.GP8,board.GP9)
+keyboard.diode_orientation = DiodeOrientation.ROW2COL
 
 keyboard.keymap = [
-    [KC.A,KC.B,KC.C]
+    [KC.A,KC.B,KC.C,
+    KC.D,KC.E,KC.F],
+    KC.G,KC.H,KC.I,
+    KC.J,KC.K,KC.L]
 ]
 
 if __name__ == '__main__':
